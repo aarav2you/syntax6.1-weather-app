7c5af3a96f835831f7d022c14c335d2d18b6959c
Aarav Prasad <aarav@pagux.com>
2022-08-14 22:33:10 +0530

    v3

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,15 @@ void main() {
   runApp(WeatherApp());
 }
 
+Icon customIcon = const Icon(
+  Icons.search,
+  size: 30,
+  color: Colors.white,
+);
+
+Widget customSearchBar = const Text('Weather App');
+
+String cityName = "Noida";
 class WeatherApp extends StatefulWidget {
   @override
   State<WeatherApp> createState() => _WeatherAppState();
@@ -25,9 +34,11 @@ String titleCase(String text) {
       .join(' ');
 }
 
+
 class _WeatherAppState extends State<WeatherApp> {
   var temp;
   var weatherSvg;
+  var weatherBackground;
   var date;
   var time;
 
@@ -46,7 +57,7 @@ class _WeatherAppState extends State<WeatherApp> {
         "https://api.open-meteo.com/v1/$cityParameter&current_weather=true&timezone=auto"));
 
     var weatherResult = json.decode(weatherReponse.body);
-    print(json.decode(weatherReponse.body));
+    print(weatherResult);
 
     Future getLocalTime(String cityTimeZone) async {
       await tz.initializeTimeZone();
@@ -62,14 +73,16 @@ class _WeatherAppState extends State<WeatherApp> {
         ? true
         : false; // Greater than 6am and less than 7pm = true
 
-    var weatherSvgName;
     var wmoCode = weatherResult["current_weather"]["weathercode"];
+    var weatherBackgroundName;
+    var weatherSvgName;
+    // WMO graph interpretation
 
     if (wmoCode == 0 && isDay) {
       weatherSvgName = "clear-day";
     } else if (wmoCode == 0 && isDay != true) {
       weatherSvgName = "clear-night";
-    } else if ((wmoCode >= 1 && wmoCode <= 3) && isDay) {
+    } else if (wmoCode >= 1 && wmoCode <= 3 && isDay) {
       weatherSvgName = "cloudy-day";
     } else if (wmoCode >= 1 && wmoCode <= 3 && isDay != true) {
       weatherSvgName = "cloudy-night";
@@ -83,27 +96,44 @@ class _WeatherAppState extends State<WeatherApp> {
       weatherSvgName = "rain4";
     } else if (wmoCode >= 71 && wmoCode <= 75) {
       weatherSvgName = "snow1";
-    } else if (wmoCode >= 77 && wmoCode <= 82) {
+    } else if (wmoCode >= 77 && wmoCode <= 77) {
       weatherSvgName = "snow2";
-    } else if (wmoCode == 86 || wmoCode == 86) {
+    } else if (wmoCode >= 80 && wmoCode <= 82) {
+      weatherSvgName = "rain3";
+    } else if (wmoCode == 85 || wmoCode == 86) {
       weatherSvgName = "snow3";
     } else {
       weatherSvgName = "thunder";
     }
+
+    // Generalizing WMO codes
+    if (wmoCode >= 0 && wmoCode <= 3 && isDay) {
+      weatherBackgroundName = "morning";
+    } else if (wmoCode >= 0 && wmoCode <= 3 && isDay != true) {
+      weatherBackgroundName = "night";
+    } else if (wmoCode >= 56 && wmoCode <= 67) {
+      weatherBackgroundName = "raining";
+    } else if (wmoCode >= 71 && wmoCode <= 77) {
+      weatherBackgroundName = "snowing";
+    } else if (wmoCode >= 80 && wmoCode <= 82) {
+      weatherBackgroundName = "raining";
+    } else if (wmoCode >= 95 && wmoCode <= 99) {
+      weatherBackgroundName = "thunder";
+    }
+
     setState(() {
       this.temp = weatherResult["current_weather"]["temperature"];
       this.weatherSvg = "assets/weather/" + weatherSvgName + ".svg";
+      this.weatherBackground = "assets/time/" + weatherBackgroundName + ".jpg";
       this.time = time.toString().split(" ")[1].split(".")[0];
       this.date = time.toString().split(" ")[0];
     });
-
-    // print(json.decode(weatherReponse.body)["current_weather"]["temperature"]);
   }
 
   @override
   void initState() {
     super.initState();
-    this.getWeather("new york");
+    this.getWeather(cityName);
   }
 
   @override
@@ -118,37 +148,119 @@ class _WeatherAppState extends State<WeatherApp> {
         home: Scaffold(
           extendBodyBehindAppBar: true,
           appBar: AppBar(
-            centerTitle: true,
-            elevation: 0,
-            title: Text("CITY!"), // Change
+            title: customSearchBar,
             backgroundColor: Colors.black38,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(20),
-              ),
-            ),
-            actions: [
-              IconButton(
-                padding: EdgeInsets.only(right: 20),
-                onPressed: null,
-                icon: Icon(
-                  Icons.search,
-                  size: 25,
-                  color: Colors.white,
-                ),
-              ),
-            ],
+            elevation: 0,
+            leading: IconButton(
+                onPressed: () {
+                  setState(() {
+                    if (customIcon.icon == Icons.search) {
+                      customIcon = const Icon(Icons.cancel);
+                      customSearchBar = ListTile(
+                        title: TextField(
+                          textInputAction: TextInputAction.go,
+                          onSubmitted: (value) {
+                            cityName = value;
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'Type in full name of city...',
+                            hintStyle: TextStyle(
+                              color: Colors.white38,
+                              fontSize: 18,
+                              fontStyle: FontStyle.italic,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    } else {
+                      customIcon = Icon(Icons.search);
+                      customSearchBar = Text(cityName);
+                    }
+                  });
+                },
+                icon: customIcon),
+            centerTitle: true,
           ),
+          // appBar: AppBar(
+          //   title: Icon(
+          //     Icons.search,
+          //     size: 30,
+          //     color: Colors.white,
+          //   ),
+          //   backgroundColor: Colors.black38,
+          //   shape: RoundedRectangleBorder(
+          //     borderRadius: BorderRadius.vertical(
+          //       bottom: Radius.circular(20),
+          //     ),
+          //   ),
+          //   leading: IconButton(
+          //     onPressed: () {
+          //       setState(() {
+          //         if (customIcon.icon == Icons.search) {
+          //           customIcon = const Icon(Icons.cancel);
+          //           customSearchBar = ListTile(
+          //             title: TextField(
+          //               textInputAction: TextInputAction.go,
+          //               //     onChanged: (text) {
+          //               //     print("City Name  is : "+ text);
+          //               // },
+          //               onSubmitted: (value) {
+          //                 cityName = value;
+          //                 print("search cityName ->>>>" + cityName);
+          //               },
+
+          //               decoration: InputDecoration(
+          //                 hintText: 'type in city name...',
+          //                 hintStyle: TextStyle(
+          //                   color: Colors.white,
+          //                   fontSize: 18,
+          //                   fontStyle: FontStyle.italic,
+          //                 ),
+          //                 border: InputBorder.none,
+          //               ),
+          //               style: TextStyle(
+          //                 color: Colors.white,
+          //               ),
+          //             ),
+          //             // title: SelectCityWidget(),
+          //           );
+          //         } else {
+          //           customIcon = const Icon(Icons.search);
+          //           customSearchBar = Text(cityName);
+          //         }
+          //       });
+          //     },
+          //     icon: customIcon,
+          //   ),
+          //   // actions: [
+          //   //   IconButton(
+          //   //     padding: EdgeInsets.only(right: 20),
+          //   //     onPressed: null,
+          //   //     icon: Icon(
+          //   //       Icons.search,
+          //   //       size: 25,
+          //   //       color: Colors.white,
+          //   //     ),
+          //   //   ),
+          //   // ],
+          // ),
           body: Container(
               child: Stack(children: [
-            Image.asset(
-              "assets/time/night.jpg", // dynamic
-              alignment: Alignment.center,
-              height: double.infinity,
-              width: double.infinity,
-              fit: BoxFit.cover,
-              filterQuality: FilterQuality.high,
-            ),
+            Container(
+                child: weatherBackground != null
+                    ? Image.asset(
+                        weatherBackground,
+                        alignment: Alignment.center,
+                        height: double.infinity,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                        filterQuality: FilterQuality.high,
+                      )
+                    : Text("Loading...")),
             Container(
               decoration: BoxDecoration(color: Colors.black45),
             ),
@@ -158,17 +270,13 @@ class _WeatherAppState extends State<WeatherApp> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
+                    padding: const EdgeInsets.only(bottom: 50),
                     child: weatherSvg != null
                         ? SvgPicture.asset(weatherSvg, width: 300)
-                        : Text("Loading"),
-                    // SvgPicture.asset(
-                    //   'assets/weather/cloudy-night.svg',
-                    //   width: 300,
-                    // ),
+                        : Text("Loading..."),
                   ),
                   Text(
-                    temp != null ? temp.toString() + "\u{2103}" : "Loading",
+                    temp != null ? temp.toString() + "\u{2103}" : "Loading...",
                     style: GoogleFonts.inter(
                         fontSize: 75,
                         fontWeight: FontWeight.bold,
@@ -176,14 +284,14 @@ class _WeatherAppState extends State<WeatherApp> {
                         height: -0.5),
                   ),
                   Text(
-                    date != null ? date : "Loading",
+                    date != null ? date : "Loading...",
                     style: GoogleFonts.inter(
                       fontSize: 30,
                       color: Colors.white,
                     ),
                   ),
                   Text(
-                    time != null ? time : "Loading",
+                    time != null ? time : "Loading...",
                     style: GoogleFonts.inter(
                       fontSize: 30,
                       color: Colors.white,
